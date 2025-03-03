091a78cc6480f0b6394b0f0c9f9f4482082ef67d
sujithaseenivasan <sujithaseenivasan@utexas.edu>
2025-03-03 15:47:08 -0600

    added firebase file + packages and login/signup UI

---
+++ beanthere/CreateAccountViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateAccountViewController.swift
+//  beanthere
+//
+//  Created by Sujitha Seenivasan on 3/3/25.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
