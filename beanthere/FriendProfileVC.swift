1b06bee89bb178af0f3e4cd45669d236820c9d84
tesi123 <yroneumutesi@gmail.com>
2025-04-07 13:48:09 -0500

    added connection to friendProfileVC

---
+++ beanthere/FriendProfileVC.swift
@@ -0,0 +1,50 @@
+//
+//  FriendProfileVC.swift
+//  beanthere
+//
+//  Created by Yrone Umutesi on 4/7/25.
+//
+
+import UIKit
+
+class FriendProfileVC: UIViewController {
+    
+    @IBOutlet weak var friendImg: UIImageView!
+    @IBOutlet weak var friendName: UILabel!
+    
+    @IBOutlet weak var friendUserName: UILabel!
+    
+    @IBOutlet weak var followersNum: UILabel!
+    @IBOutlet weak var followingNum: UILabel!
+    
+    @IBOutlet weak var beenNum: UILabel!
+    @IBOutlet weak var wantNum: UILabel!
+    
+    @IBOutlet weak var friendReviewTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func followButton(_ sender: Any) {
+    }
+    
+    
+    
+    @IBAction func beenBrewButton(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
