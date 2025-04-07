35f004f0bbe53fe58c12d2aa7e8bca335fa9a2f6
eshikohli <eshi.kohli@gmail.com>
2025-04-07 11:18:54 -0500

    finished UI and started backend

---
+++ beanthere/FriendRequestsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FriendRequestsViewController.swift
+//  beanthere
+//
+//  Created by Eshi Kohli on 4/7/25.
+//
+
+import UIKit
+
+//class FriendRequestTableViewCell: UITableViewCell {
+//    
+//    
+//}
+
+class FriendRequestsViewController: UIViewController {
+
+    @IBOutlet weak var friendRequestsTable: UITableView!
+    
+    @IBOutlet weak var requestNameLabel: UILabel!
+    
+    @IBOutlet weak var requestUsernameLabel: UILabel!
+    
+    var delegate: UIViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func confirmRequest(_ sender: Any) {
+    }
+    
+    
+    @IBAction func deleteRequest(_ sender: Any) {
+    }
+}
---
+++ beanthere/FriendsConnectViewController.swift
@@ -0,0 +1,52 @@
+//
+//  FriendsConnectViewController.swift
+//  beanthere
+//
+//  Created by Eshi Kohli on 4/7/25.
+//
+
+import UIKit
+
+class FriendsConnectViewController: UIViewController {
+    
+    
+    @IBOutlet weak var suggestFriendsCollection: UICollectionView!
+    
+    @IBOutlet weak var contactsFriendsCollection: UICollectionView!
+    
+    @IBOutlet weak var suggestedFriendImage: UIImageView!
+    
+    
+    @IBOutlet weak var suggestedFriendName: UILabel!
+    
+    @IBOutlet weak var suggestedFriendUsername: UILabel!
+    
+    
+    @IBOutlet weak var contactsFriendImage: UIImageView!
+    
+    
+    @IBOutlet weak var contactsFriendName: UILabel!
+    
+    
+    @IBOutlet weak var contactsFriendUsername: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ViewRequests",
+           let nextVC = segue.destination as? FriendRequestsViewController {
+            nextVC.delegate = self
+        }
+    }
+    
+    
+    @IBAction func followSuggestedFriend(_ sender: Any) {
+    }
+    
+    
+    @IBAction func followContactsFriend(_ sender: Any) {
+    }
+    
+}
