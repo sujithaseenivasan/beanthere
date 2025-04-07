7477cfdcb641e8d5a3e6f95dfa6199ad1e7803c0
sarahfedorchak <sarahfedorchak@utexas.edu>
2025-04-07 17:23:54 -0500

    working on tabs stuff for brew log

---
+++ beanthere/BrewTabsViewController.swift
@@ -0,0 +1,52 @@
+//
+//  BrewTabsViewController.swift
+//  beanthere
+//
+//  Created by Sarah Neville on 4/7/25.
+//
+
+import UIKit
+
+class BrewTabsViewController: UIViewController {
+
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    var beenVC: BrewLogViewController!
+    var wantToTryVC: WantToTryViewController!
+    //TODO: need to make variable for recs when i make that screen
+    var currentVC: UIViewController?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let storyboard = UIStoryboard(name: "UserSettings", bundle: nil)
+        
+        //load the VCs programatically
+        beenVC = storyboard.instantiateViewController(withIdentifier: "BeenViewController") as? BrewLogViewController
+        wantToTryVC = storyboard.instantiateViewController(withIdentifier: "WantToTryViewController") as? WantToTryViewController
+        
+        switchToVC(beenVC)
+        
+        
+    }
+    
+    @IBAction func segmentChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+            case 0:
+                switchToVC(beenVC)
+            case 1:
+                switchToVC(wantToTryVC)
+            case 2:
+                print("Not created yet")
+            default:
+                break
+            }
+    }
+    
+    func switchToVC(_ vc: UIViewController) {
+        //TODO: add code here
+    }
+
+
+}
