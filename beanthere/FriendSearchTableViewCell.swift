0c40134604b83da2f0d1716014d78d2bd8e546a0
eshikohli <eshi.kohli@gmail.com>
2025-04-09 06:09:10 -0500

    added baseline for friend search

---
+++ beanthere/FriendSearchTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  FriendSearchTableViewCell.swift
+//  beanthere
+//
+//  Created by Eshi Kohli on 4/9/25.
+//
+
+import UIKit
+
+class FriendSearchTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var friendSearchName: UILabel!
+    
+
+    @IBOutlet weak var friendSearchUsername: UILabel!
+    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
