6078aa2317f9016532cb237199c67c3cdc6896c1
sarahfedorchak <sarahfedorchak@utexas.edu>
2025-04-26 21:19:57 -0500

    fixed dark mode stuff

--- beanthere/SceneDelegate.swift
+++ beanthere/SceneDelegate.swift
@@ -13,10 +13,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = scene as? UIWindowScene else { return }
+
+        for window in windowScene.windows {
+            let isDarkMode = UserDefaults.standard.bool(forKey: "isDarkModeEnabled")
+            let style: UIUserInterfaceStyle = isDarkMode ? .dark : .light
+            
+            UIView.transition(with: window, duration: 0.5, options: [.transitionCrossDissolve], animations: {
+                window.overrideUserInterfaceStyle = style
+            }, completion: nil)
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
